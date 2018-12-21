57f5dc881e22dc324dbdc479c67003721bc3e437
val <valgrinberg@hotmail.com>
2018-12-20 21:07:15 -0800

    additional change

---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaService.java
@@ -0,0 +1,68 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.repositories.OwnerRepository;
+import guru.springframework.sfgpetclinic.repositories.PetRepository;
+import guru.springframework.sfgpetclinic.repositories.PetTypeRepository;
+import guru.springframework.sfgpetclinic.services.OwnerService;
+
+@Service
+@Profile("springdatajpa")
+public class OwnerSDJpaService implements OwnerService{
+
+	private final OwnerRepository ownerRepository;
+	private final PetRepository petRepository;
+	private final PetTypeRepository petTypRepository;
+	
+	public OwnerSDJpaService(OwnerRepository ownerRepository, PetRepository petRepository,
+			PetTypeRepository petTypRepository) {
+		
+		this.ownerRepository = ownerRepository;
+		this.petRepository = petRepository;
+		this.petTypRepository = petTypRepository;
+	}
+
+
+	@Override
+	public Owner findByLastName(String lastName) {
+		return (Owner) ownerRepository.findByLastName(lastName);
+	}
+	
+	@Override
+	public Set<Owner> findAll() {
+		
+		Set<Owner> owners = new HashSet<>();
+		ownerRepository.findAll().forEach(owners::add);
+		
+		return owners;		 
+	}
+
+	@Override
+	public Owner findById(Long aLong) {		
+		return ownerRepository.findById(aLong).orElse(null);
+	}
+
+	@Override
+	public Owner save(Owner object) {
+		return ownerRepository.save(object);
+	}
+
+	@Override
+	public void delete(Owner object) {
+		ownerRepository.delete(object);
+	}
+
+	@Override
+	public void deleteById(Long aLong) {
+		ownerRepository.deleteById(aLong);
+	}
+
+
+
+}
