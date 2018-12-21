779821e39ba0066181c335c17bb508cd0ee28df1
val <valgrinberg@hotmail.com>
2018-12-20 20:39:44 -0800

    added JPA Repositories,closes #38

---
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgpetclinic.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+
+public interface OwnerRepository <Owner, Long>{
+
+}
