901cc5b23e6560145390aaed6d390b0f5e59d8f9
val <valgrinberg@hotmail.com>
2019-01-10 18:12:18 -0800

    added vets api response

--- sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import java.util.Set;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.VetService;
 
 
@@ -22,5 +27,10 @@ public class VetController {
 		model.addAttribute("vets", vetService.findAll());
 		
 		return "vets/index";
+	}
+	
+	@GetMapping("/api/vets")
+	public @ResponseBody Set<Vet> getVetsJson() {
+		return vetService.findAll();		
 	}
 }
